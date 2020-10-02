0ac31e341c1ae5d1bc46019e9a87f66795a8a747
Alfonso <alfof87@gmail.com>
2020-10-02 16:31:07 +0200

    laravel-relations

--- MyLaravel/routes/web.php
+++ MyLaravel/routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'EmployeeController@index') -> name('emp.index');
-Route::get('/show', 'EmployeeController@show') -> name('emp.show');
+Route::get('/show/{id}', 'EmployeeController@show') -> name('emp.show');
