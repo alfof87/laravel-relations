37173d516f9978edc466b5d834836d2339d297d8
Alfonso <alfof87@gmail.com>
2020-10-04 16:42:01 +0200

    laravel-relations

--- MyLaravel/resources/views/layouts/show.blade.php
+++ MyLaravel/resources/views/layouts/show.blade.php
@@ -3,6 +3,9 @@
   <div class="flex">
     <ul>
         <li>
+          <img src="https://brighterwriting.com/wp-content/uploads/icon-user-default.png" alt="">
+        </li>
+        <li>
           <h4>Date of Birth:</h4>
           {{ $emp -> date_of_birth }}
         </li>
